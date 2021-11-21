b026080ecb8f0aac009fcfe88433f71ad4bdfb93
Yuva Rani <yuvaranibu@gmail.com>
2021-11-20 21:52:40 -0500

    First file

---
+++ main.py
@@ -0,0 +1,27 @@
+"""x = 30;
+
+y= 50;
+z =y;
+
+print("x =",x)
+print("y =",y)
+print("z =",z)
+
+print("id of x =",id(x))
+print("id of z =",id(z))
+print("id of y =",id(y))"""
+## Program for practicing if else in python
+## Checking if, elif,else
+
+#n = int(input("Enter an Interger"))
+n = input("Enter an Interger")
+if n == 0:
+    print("The number entered is 0")
+elif n>0:
+    print("The number entered is greater than 0")
+elif n<0:
+    print("The number entered is less than 0")
+else:
+    print("You will never reach else part")
+
+
